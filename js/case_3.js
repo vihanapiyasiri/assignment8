d8c21023e0a1bcd981705e6019778c0d1315b16d
vihanapiyasiri <157113873+vihanapiyasiri@users.noreply.github.com>
2024-11-11 20:10:53 +0530

    new

--- js/case_3.js
+++ js/case_3.js
@@ -1,60 +1,56 @@
-// const startBtn = document.getElementById("startBtn");
-// const stopBtn = document.getElementById("stopBtn");
-// const knightRiderSound = document.getElementById("knightRiderSound");
-let isRunning = false;
-      let intervalId;
-      let currentIndex = 0;
-      let direction = 1;
 
-      startBtn.onclick = function () {
-        if (!isRunning) {
-          // knightRiderSound.play();
-          // knightRiderSound.loop = true; 
-          intervalId = setInterval(function () {
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const knightRiderAudio = document.getElementById("knightRiderAudio");
+
+let isRunning = false;
+let intervalId;
+let currentIndex = 0;
+let direction = 1;
+
+startBtn.onclick = function () {
+    if (!isRunning) {
+        knightRiderAudio.play(); 
+        knightRiderAudio.loop = true; 
+
+        intervalId = setInterval(function () {
             let cards = document.getElementsByClassName("card");
+
+           
             for (let i = 0; i < cards.length; i++) {
-              cards[i].style.backgroundColor = "white";
+                cards[i].style.backgroundColor = "white";
             }
 
             cards[currentIndex].style.backgroundColor = "#ff4d4d";
             if (currentIndex > 0) {
-              cards[currentIndex - 1].style.backgroundColor = "#ff9f9e";
+                cards[currentIndex - 1].style.backgroundColor = "#ff9f9e";
             }
             if (currentIndex < cards.length - 1) {
-              cards[currentIndex + 1].style.backgroundColor = "#ff9f9e";
+                cards[currentIndex + 1].style.backgroundColor = "#ff9f9e";
             }
 
-            currentIndex = currentIndex + direction;
+            
+            currentIndex += direction;
             if (currentIndex >= cards.length - 1) {
-              direction = -1;
+                direction = -1; 
             } else if (currentIndex <= 0) {
-              direction = 1;
+                direction = 1; 
             }
-          }, 150);
+        }, 150);
 
-          isRunning = true;
-          startBtn.style.backgroundColor = "#ff4d4d";
-          stopBtn.style.backgroundColor = "white";
-        }
-      };
+        isRunning = true;
+        startBtn.style.backgroundColor = "#ff4d4d";
+        stopBtn.style.backgroundColor = "white";
+    }
+};
 
-      stopBtn.onclick = function () {
-        if (isRunning) {
-          // knightRiderSound.pause();
-          // knightRiderSound.currentTime = 0;
-          clearInterval(intervalId);
-          isRunning = false;
-          stopBtn.style.backgroundColor = "#ff4d4d";
-          startBtn.style.backgroundColor = "white";
-        }
-      };
-
-      stopBtn.onclick = function () {
-        if (isRunning) {
-          knightRiderAudio.pause();
-          clearInterval(intervalId);
-          isRunning = false;
-          stopBtn.style.backgroundColor = "#ff4d4d";
-          startBtn.style.backgroundColor = "white";
-        }
-      }; 
+stopBtn.onclick = function () {
+    if (isRunning) {
+        knightRiderAudio.pause(); 
+        knightRiderAudio.currentTime = 0; 
+        clearInterval(intervalId); 
+        isRunning = false;
+        stopBtn.style.backgroundColor = "#ff4d4d";
+        startBtn.style.backgroundColor = "white";
+    }
+};
